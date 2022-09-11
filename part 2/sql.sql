5df760987ce61442bd5d5acbe7b6f2d866bbc0cf
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-11 23:07:10 +0300

    selecting bif courses

--- part 2/sql.sql
+++ part 2/sql.sql
@@ -10,3 +10,9 @@ WHERE cappacity >= 100;
 SELECT * 
 FROM `Course`
 where start_time = (SELECT MIN(start_time) FROM Course);
+
+-- Find all courses taken by BIF majors
+
+SELECT DISTINCT crn
+FROM `Enrolled` 
+WHERE Enrolled.Student_id = ANY(SELECT Student_id from MajorsIn WHERE MajorsIn.Department_id = 1);
