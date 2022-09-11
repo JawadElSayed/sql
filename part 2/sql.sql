430ab85cf2e929fda838b1e996c7660ddd406c60
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-11 22:00:43 +0300

    selecting first course

--- part 2/sql.sql
+++ part 2/sql.sql
@@ -1,8 +1,12 @@
 
-
 -- Find all rooms that can seat at least 100 people
 
 SELECT *
 from Room
 WHERE cappacity >= 100;
 
+-- Find the course or courses with the earliest start time
+
+SELECT * 
+FROM `Course`
+where start_time = (SELECT MIN(start_time) FROM Course);
