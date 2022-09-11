400be1d092054d6a87b53c19164d46060679dae5
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-11 23:29:52 +0300

    selecting not enrolled students

--- part 2/sql.sql
+++ part 2/sql.sql
@@ -16,3 +16,9 @@ where start_time = (SELECT MIN(start_time) FROM Course);
 SELECT DISTINCT crn
 FROM `Enrolled` 
 WHERE Enrolled.Student_id = ANY(SELECT Student_id from MajorsIn WHERE MajorsIn.Department_id = 1);
+
+-- Create a list of all students who are not enrolled in a course
+
+SELECT DISTINCT * 
+FROM Student
+WHERE Student.id NOT IN (SELECT Student_id FROM Enrolled);
